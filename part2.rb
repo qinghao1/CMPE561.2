9ed3a9a27fe282991705f91b1d0d001a76bcdf1c
qinghao1 <qinghao1+github@gmail.com>
2017-12-16 16:40:53 +0300

    final

--- part2.rb
+++ part2.rb
@@ -57,34 +57,77 @@ File.open('test.txt', 'w') do |file|
 	end
 end
 
-# test_lines.each do |line|
-# 	language = line[-4..-1];
-# 	letters_regex = Regexp.new('^[[:alpha:]]'.encode('UTF-16LE'))
-# 	line2 = line.gsub(letters_regex,'')
-# 	language_id = language_id_map[language];
-# 	test_letters_map[language_id] ||= [];
-# 	test_letters_map[language_id].push(SortedSet.new)
-# 	line2.each_char do |char|
-# 		test_letters_map[language_id].last.add(char.ord)
-# 	end
-# end
-# language_id_map.each do |language_str, language_id|
-# 	utf_8_language_str = language_str[0..1].encode(Encoding.find('UTF-8'), {invalid: :replace, undef: :replace, replace: ''})
-# 	sentence_array = test_letters_map[language_id]
-# 	File.open("test_#{utf_8_language_str}.txt", 'w') do |file|
-# 		sentence_array.each do |character_set|
-# 			file.write "#{language_id} "
-# 			character_set.each{|c| file.write("#{c}:1 ")}
-# 			file.write "\n"
-# 		end
-# 	end
-# end
-
 #Run SVM
-# C_VALUE = 1.0
-# if `./svm_multiclass_learn -c #{C_VALUE} training.txt learned_svm` != 0 
-# 	puts "Error: Can't run svm_multiclass_learn"
-# end
-# if `./svm_multiclass_classify test.txt learned_svm output.txt` != 0
-# 	puts "Error: Can't run svm_multiclass_classify"
-# end
+C_VALUE = 10.0
+puts `./svm_multiclass_learn -c #{C_VALUE} training.txt learned_svm`
+puts `./svm_multiclass_classify test.txt learned_svm output.txt`
+
+#Read test and output files to get statistics
+statistics = {total: {TP:0, FP:0, FN:0, TN:0}}
+language_id_map.values.each do |id|
+	statistics[id] = {
+		TP: 0,
+		FP: 0,
+		FN: 0,
+		TN: 0
+	}
+end
+test_file = File.open('test.txt', 'r')
+output_file = File.open('output.txt', 'r')
+test_file.each.zip(output_file.each).each do |test_line, output_line|
+	test_id = test_line.split.first.to_i
+	output_id = output_line.split.first.to_i
+	if test_id == output_id #Correct classification
+		statistics[output_id][:TP] += 1
+		statistics[:total][:TP] += 1
+		statistics.each do |id, individual_stats| #Increment TN value of all other languages
+			if id != output_id
+				individual_stats[:TN] += 1 
+				statistics[:total][:TN] += 1
+			end
+		end
+	else #Incorrect classification
+		statistics[test_id][:FN] += 1
+		statistics[:total][:FN] += 1
+		statistics[output_id][:FP] += 1
+		statistics[:total][:FP] += 1
+	end
+end
+test_file.close and output_file.close
+
+File.open('stats.txt', 'w') do |f|
+	pi = rho = big_f = 0
+	f.puts "Macro-averaged Precision, Recall, F-measure"
+	statistics.each do |id, stats|
+		next if id == :total
+		language_str = String.new
+		language_id_map.each do |str, l_id|
+			if id == l_id
+				language_str = str[0..1].encode(Encoding.find('UTF-8'), {invalid: :replace, undef: :replace, replace: ''})
+				break
+			end
+		end
+		f.puts "#{language_str} - TPi:#{stats[:TP]} FPi:#{stats[:FP]} FNi:#{stats[:FN]} TNi:#{stats[:TN]}"
+		pi_i = stats[:TP].to_f / (stats[:TP] + stats[:FP])
+		rho_i = stats[:TP].to_f / (stats[:TP] + stats[:FN])
+		f_i = (2 * pi_i * rho_i) / (pi_i + rho_i)
+		f.puts "#{language_str} - pi_i:#{'%.5f' % pi_i} rho_i:#{'%.5f' % rho_i} F_i:#{'%.5f' % f_i}"
+		pi += pi_i
+		rho += rho_i
+		big_f += f_i
+	end
+	pi /= 13
+	rho /= 13
+	big_f /= 13
+	f.puts "OVERALL - pi:#{'%.5f' % pi} rho:#{'%.5f' % rho} F:#{'%.5f' % big_f}"
+	pi = rho = big_f = 0
+
+	f.puts
+	f.puts "Micro-averaged Precision, Recall, F-measure"
+	puts statistics[:total][:FP]
+	puts statistics[:total][:FN]
+	pi = statistics[:total][:TP].to_f / (statistics[:total][:TP] + statistics[:total][:FP])
+	rho = statistics[:total][:TP].to_f / (statistics[:total][:TP] + statistics[:total][:FN])
+	big_f = (2 * pi * rho) / (pi + rho)
+	f.puts "OVERALL - pi: #{'%.5f' % pi} rho: #{'%.5f' % rho} F: #{'%.5f' % big_f}"
+end
